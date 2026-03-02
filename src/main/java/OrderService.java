e9ef7b18cef96cce064fe75b0a15630938adb326
unknown <akalyaanandhan@gmail.com>
2026-03-02 11:48:44 +0530

    Initial commit

---
+++ src/main/java/OrderService.java
@@ -0,0 +1,11 @@
+public class OrderService {
+
+    public static void main(String[] args) {
+        System.out.println("E-commerce Order Service Running...");
+        createOrder();
+    }
+
+    public static void createOrder() {
+        System.out.println("Order created successfully!");
+    }
+}
