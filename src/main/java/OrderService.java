3511d5917be045ab906ca1fab8837c98888cd65a
unknown <akalyaanandhan@gmail.com>
2026-03-02 12:15:58 +0530

    Added payment feature

--- src/main/java/OrderService.java
+++ src/main/java/OrderService.java
@@ -2,6 +2,7 @@ public class OrderService {
 
     public static void main(String[] args) {
         System.out.println("E-commerce Order Service Running...");
+        System.out.println("Payment feature added");
         createOrder();
     }
 
